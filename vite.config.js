a707d295ca556bda3f5bd6e96996acc1ea7cc123
Naflan <naflan265@gmail.com>
2025-10-08 00:47:36 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/chatbot.css',
+                'resources/css/sidebar.css',
                 'resources/css/app.css',
                 'resources/js/app.js'
                 ],
